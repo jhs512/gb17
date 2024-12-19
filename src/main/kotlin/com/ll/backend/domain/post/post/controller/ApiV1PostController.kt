d798eb95d4c77d9fdf31be2c49edaf86a4fdcbcf
jhs512 <jangka512@gmail.com>
2024-12-19 23:59:30 +0900

    t1, g

---
+++ src/main/kotlin/com/ll/backend/domain/post/post/controller/ApiV1PostController.kt
@@ -0,0 +1,22 @@
+package com.ll.backend.domain.post.post.controller
+
+import com.ll.backend.domain.post.post.dto.PostDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/posts")
+class ApiV1PostController {
+    @GetMapping("/{id}")
+    fun getItem(
+        @PathVariable id: Long
+    ): PostDto {
+        return PostDto(
+            id = 1,
+            title = "title",
+            body = "body"
+        )
+    }
+}
---
+++ src/main/kotlin/com/ll/backend/domain/post/post/dto/PostDto.kt
@@ -0,0 +1,9 @@
+package com.ll.backend.domain.post.post.dto
+
+data class PostDto(
+    val id: Int,
+    val title: String,
+    val body: String
+) {
+
+}
