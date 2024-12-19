96dcb2e93d2f191fcf7d440150b35afa463e5d04
jhs512 <jangka512@gmail.com>
2024-12-19 23:53:45 +0900

    t1, r

---
+++ src/test/kotlin/com/ll/backend/domain/post/post/controller/ApiV1PostControllerTest.kt
@@ -0,0 +1,40 @@
+package com.ll.backend.domain.post.post.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.DisplayName
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import kotlin.test.Test
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApiV1PostControllerTest @Autowired constructor(
+    private val mockMvc: MockMvc,
+    private val objectMapper: ObjectMapper
+) {
+    @Test
+    @DisplayName("GET /api/v1/posts/1")
+    fun t1() {
+        // WHEN
+        val resultActions = mockMvc
+            .perform(
+                get("/api/v1/posts/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+
+        // THEN
+        resultActions
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id").value(1))
+            .andExpect(jsonPath("$.title").exists())
+            .andExpect(jsonPath("$.body").exists())
+    }
+}
