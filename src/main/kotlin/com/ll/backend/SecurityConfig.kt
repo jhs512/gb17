a94c57a43a72891fcba88761d52b0c7867c988dd
jhs512 <jangka512@gmail.com>
2024-12-19 23:41:21 +0900

    allow all request, sameOrigin=true, disable csrf, cors config

---
+++ src/main/kotlin/com/ll/backend/SecurityConfig.kt
@@ -0,0 +1,41 @@
+package com.ll.backend
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        httpSecurity {
+            authorizeHttpRequests {
+                authorize("/**", permitAll)
+            }
+
+            headers {
+                frameOptions {
+                    sameOrigin = true
+                }
+            }
+
+            csrf { disable() }
+        }
+
+        return httpSecurity.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("https://cdpn.io")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/api/**", configuration)
+        return source
+    }
+}
